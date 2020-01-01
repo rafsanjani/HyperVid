8cd613088202916f6659f016d1b008e27485301b
rafsanjani <coded_raf@yahoo.com>
2020-01-01 23:46:07 +0000

    Rename parameter for downloader interface

--- downloader/src/main/java/com/foreverrafs/downloader/Downloader.kt
+++ downloader/src/main/java/com/foreverrafs/downloader/Downloader.kt
@@ -3,7 +3,7 @@ package com.foreverrafs.downloader
 import com.foreverrafs.downloader.model.DownloadInfo
 
 interface Downloader {
-    fun downloadFile(downloadInfo: DownloadInfo, listener: DownloadEvents): Int
+    fun downloadFile(downloadInfo: DownloadInfo, videoDownloadListener: DownloadEvents): Int
     fun pauseDownload(downloadId: Int): Boolean
     fun cancelDownload(downloadId: Int): Boolean
 }
