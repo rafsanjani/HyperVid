a6155ec32df142a2468f0ec9e758346ad96a6365
rafsanjani <coded_raf@yahoo.com>
2020-01-02 22:19:05 +0000

    Add implementation for videos adapter

--- app/src/main/java/com/foreverrafs/rdownloader/adapter/VideosAdapter.kt
+++ app/src/main/java/com/foreverrafs/rdownloader/adapter/VideosAdapter.kt
@@ -1,26 +1,43 @@
 package com.foreverrafs.rdownloader.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.foreverrafs.rdownloader.R
+import com.foreverrafs.rdownloader.model.FacebookVideo
 
-class VideosAdapter private constructor() :
-    RecyclerView.Adapter<VideosAdapter.VideosViewHolder>() {
+class VideosAdapter : RecyclerView.Adapter<VideosAdapter.VideosViewHolder>() {
+    val videoList = mutableListOf<FacebookVideo>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideosViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_video__, parent, false)
+        return VideosViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return videoList.size
     }
 
     override fun onBindViewHolder(holder: VideosViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(videoList[position])
+    }
+
+    fun addVideo(facebookVideo: FacebookVideo) {
+        videoList.add(facebookVideo)
+        notifyItemInserted(videoList.size)
+    }
+
+    fun removeVideo(position: Int) {
+        videoList.removeAt(position)
+        notifyItemRemoved(position)
     }
 
 
     inner class VideosViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(facebookVideo: FacebookVideo) {
+
+        }
 
     }
 
