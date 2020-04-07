32be366d601fb2efd60c4b499f1a323e0cb7d97d
Aziz Rafsanjani <coded_raf@yahoo.com>
2020-04-07 08:23:27 +0100

    Store downloaded video in device gallery

--- app/src/main/java/com/foreverrafs/hyperdownloader/ui/downloads/DownloadsFragment.kt
+++ app/src/main/java/com/foreverrafs/hyperdownloader/ui/downloads/DownloadsFragment.kt
@@ -1,11 +1,17 @@
 package com.foreverrafs.hyperdownloader.ui.downloads
 
+import android.content.ContentValues
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -18,6 +24,10 @@ import com.foreverrafs.hyperdownloader.model.FacebookVideo
 import com.foreverrafs.hyperdownloader.util.invisible
 import com.foreverrafs.hyperdownloader.util.visible
 import kotlinx.android.synthetic.main.fragment_downloads.*
+import timber.log.Timber
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
 
 class DownloadsFragment private constructor() : Fragment(), DownloadAdapter.Events {
     private var downloadsAdapter = DownloadAdapter(this)
@@ -104,6 +114,7 @@ class DownloadsFragment private constructor() : Fragment(), DownloadAdapter.Even
         }
 
         if (!downloadExists) {
+            saveVideoToGallery(video)
             videosList.add(video)
             vm.setVideosList(videosList)
 
@@ -114,8 +125,83 @@ class DownloadsFragment private constructor() : Fragment(), DownloadAdapter.Even
 
             }, 2000)
         } else {
-            Toast.makeText(requireContext(), getString(R.string.duplcate_video), Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), getString(R.string.duplcate_video), Toast.LENGTH_SHORT)
+                .show()
         }
+    }
+
+    @RequiresApi(29)
+    private fun saveVideoToGalleryQ(videoFile: File, title: String): Uri? {
+        val contentResolver = activity?.contentResolver
+
+        val values = ContentValues().apply {
+            put(MediaStore.MediaColumns.DATE_TAKEN, System.currentTimeMillis())
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM + videoFile.path)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            put(MediaStore.MediaColumns.IS_PENDING, 1)
+            put(MediaStore.MediaColumns.DISPLAY_NAME, title)
+            put(MediaStore.MediaColumns.TITLE, title)
+        }
+
+        val uri = contentResolver?.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values)
+
+        contentResolver?.let {
+            val stream = contentResolver.openOutputStream(uri!!)
+
+            val fileStream = FileInputStream(videoFile)
+
+            stream?.apply {
+                write(fileStream.readBytes())
+                close()
+            }
+
+            fileStream.close()
+
+            values.clear()
+            values.put(MediaStore.MediaColumns.IS_PENDING, 0)
+            val update =
+                contentResolver.update(uri, values, null, null)
+
+            Timber.d(update.toString())
+
+            return uri
+        } ?: return null
+    }
+
+    private fun saveVideoToGallery(video: FacebookVideo): Uri? {
+
+        var uri: Uri? = null
+        try {
+            uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                saveVideoToGalleryQ(File(video.path), video.title)
+            else
+                saveVideoToGallery(File(video.path), video.title)
+        } catch (exception: IOException) {
+            Timber.e(exception)
+        }
+
+        Timber.i("Saved Video to Gallery, Uri: $uri")
+        return uri
+    }
+
+    private fun saveVideoToGallery(videoFile: File, title: String): Uri? {
+
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.TITLE, title)
+            put(MediaStore.Images.Media.DISPLAY_NAME, title)
+            put(MediaStore.Images.Media.DESCRIPTION, "description")
+            put(MediaStore.Images.Media.MIME_TYPE, "video/mp4")
+            put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis())
+            put(MediaStore.Images.Media.DATA, videoFile.toString())
+        }
+
+        val uri = requireActivity().contentResolver.insert(
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+            values
+        )
+
+        Timber.i("Saved Video to Gallery, Uri: $uri")
+        return uri
     }
 
     /**
