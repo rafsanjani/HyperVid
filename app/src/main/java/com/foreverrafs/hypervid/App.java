2a4724ad02e6c7173732da7430385e99fd88f0d9
Aziz Rafsanjani <coded_raf@yahoo.com>
2020-04-10 15:34:01 +0100

    Prepare for initial release

--- app/src/main/java/com/foreverrafs/hypervid/App.java
+++ app/src/main/java/com/foreverrafs/hypervid/App.java
@@ -2,12 +2,15 @@ package com.foreverrafs.hypervid;
 
 import android.app.Application;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import timber.log.Timber;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
