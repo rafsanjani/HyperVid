9634f7aa45e6d350e2ff0adf9f91ef858a7b3e36
Aziz Rafsanjani <coded_raf@yahoo.com>
2020-04-22 11:25:07 +0100

    Update MainActivityTest to reflect change

--- app/src/androidTest/java/com/foreverrafs/hypervid/MainActivityTest.kt
+++ app/src/androidTest/java/com/foreverrafs/hypervid/MainActivityTest.kt
@@ -1,8 +1,7 @@
 package com.foreverrafs.hypervid
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -44,15 +43,17 @@ class MainActivityTest {
     }
 
     @Test
+    fun test_enableAddButtonWhenValidLinkIsTypedMobile() {
+        onView(withId(R.id.btnAddToDownloads)).check(matches(not(isEnabled())))
+        onView(withHint(R.string.hint_url)).perform(typeText("https://m.facebook.com/story.php?story_fbid=2220724671544723&id=100008216348411"))
+        onView(withId(R.id.btnAddToDownloads)).check(matches(isEnabled()))
+    }
+
+    @Test
     fun test_disableAddButtonWhenInvalidLinkIsTyped() {
         onView(withId(R.id.btnAddToDownloads)).check(matches(not(isEnabled())))
         onView(withHint(R.string.hint_url)).perform(typeText("https://www.foreverrafs.com/Magraheb/videos/228399154931246/?t=0"))
         onView(withId(R.id.btnAddToDownloads)).check(matches(not(isEnabled())))
     }
 
-    @Test
-    fun test_extractSimpleVideoUrl() {
-        onView(withHint(R.string.hint_url)).perform(typeText("https://www.facebook.com/Magraheb/videos/228399154931246/?t=0"))
-        onView(withId(R.id.btnAddToDownloads)).perform(click())
-    }
 }
