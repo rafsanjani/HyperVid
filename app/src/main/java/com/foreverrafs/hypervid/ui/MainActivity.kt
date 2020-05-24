84eaf67096ae711db84b8c5fe042d5872c49f207
Aziz Rafsanjani <coded_raf@yahoo.com>
2020-05-25 00:46:17 +0100

    Update dependencies and AGP

--- app/src/main/java/com/foreverrafs/hypervid/ui/MainActivity.kt
+++ app/src/main/java/com/foreverrafs/hypervid/ui/MainActivity.kt
@@ -3,16 +3,16 @@ package com.foreverrafs.hypervid.ui
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import androidx.activity.invoke
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.foreverrafs.downloader.downloader.VideoDownloader
 import com.foreverrafs.hypervid.R
 import com.foreverrafs.hypervid.adapter.HomePagerAdapter
+import androidx.activity.invoke
 import com.foreverrafs.hypervid.ui.add.AddUrlFragment
 import com.foreverrafs.hypervid.ui.downloads.DownloadsFragment
 import com.foreverrafs.hypervid.ui.videos.VideosFragment
@@ -23,15 +23,17 @@ import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: MainViewModel
 
-    private val requestStorPermission: ActivityResultLauncher<String> =
+    val requestStoragePermission =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-            if (!granted) {
-                finish()
+            if (granted) {
+
+            } else {
+
             }
         }
 
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +61,8 @@ class MainActivity : AppCompatActivity() {
             if (downloads.isNotEmpty()) {
                 tabLayout.getTabAt(1)?.getOrCreateBadge()?.apply {
                     isVisible = true
-                    backgroundColor = R.color.colorPrimary
+                    backgroundColor =
+                        ContextCompat.getColor(applicationContext, R.color.colorPrimary)
                     number = downloads.size
                 }
             } else {
@@ -71,7 +74,8 @@ class MainActivity : AppCompatActivity() {
             if (videos.isNotEmpty()) {
                 tabLayout.getTabAt(2)?.getOrCreateBadge()?.apply {
                     isVisible = true
-                    backgroundColor = R.color.colorPrimary
+                    backgroundColor =
+                        ContextCompat.getColor(applicationContext, R.color.colorPrimary)
                     number = videos.size
                 }
             } else {
@@ -88,7 +92,7 @@ class MainActivity : AppCompatActivity() {
             .setTitle(getString(R.string.title_copyright_notice))
             .setPositiveButton(android.R.string.ok) { _, _ ->
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestStorPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestStoragePermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     viewModel.isFirstRun = false
                 }
             }
