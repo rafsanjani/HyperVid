bf4a4fc50cd0f2a3a69800a3290d19e168833ff5
Rafsanjani Aziz <coded_raf@yahoo.com>
2021-06-06 10:51:28 +0100

    Implement a better video duration calculation logic

--- app/src/main/java/com/foreverrafs/hypervid/util/Tools.kt
+++ app/src/main/java/com/foreverrafs/hypervid/util/Tools.kt
@@ -1,16 +1,24 @@
 package com.foreverrafs.hypervid.util
 
-import java.util.concurrent.TimeUnit
+import java.time.Duration
+import java.util.*
 
-fun getDurationString(duration: Long): String {
-    return try {
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
-        val seconds = duration % minutes.toInt()
+fun getDurationString(millis: Long): String {
+    val duration = Duration.ofMillis(millis)
 
-        "${String.format("%02d", minutes)}:${String.format("%02d", seconds)}"
-    } catch (exception: ArithmeticException) {
-        val seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
+    val hours = duration.toHours()
+    val minutes = duration.minusHours(hours).toMinutes()
+    val seconds = duration.minusHours(hours).minusMinutes(minutes).seconds
 
-        String.format("00:%02d", seconds)
+    if (hours != 0L) {
+        return "${hours.to2dp}:${minutes.to2dp}:${seconds.to2dp}"
     }
+
+    if (minutes != 0L)
+        return "${minutes.to2dp}:${seconds.to2dp}"
+
+    return "00:${seconds.to2dp}"
 }
+
+private val Long.to2dp
+    get() = String.format(Locale.ENGLISH, "%02d", this)
